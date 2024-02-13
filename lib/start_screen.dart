a0636b83dd0e2d860ae180c02cc53a35cd0154e0
CharanMN7_pc <maverickid247@gmail.com>
2024-02-13 12:59:47 +0530

    added the start screen and the image

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -15,12 +15,29 @@ class _StartScreenState extends State<StartScreen> {
     return SizedBox(
       width: double.infinity,
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text("Test your Flutter prowess on the go!"),
+          Image.asset(
+            "assets/images/flutterfire_logo.png",
+            width: 350,
+          ),
+          const Text(
+            "Test your Flutter prowess on the go!",
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.blueAccent,
+            ),
+          ),
+          const SizedBox(
+            height: 25,
+          ),
           ElevatedButton.icon(
             onPressed: () {},
             icon: const Icon(Icons.chevron_right_rounded),
             label: const Text("Start Quiz"),
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.blueAccent,
+            ),
           )
         ],
       ),
